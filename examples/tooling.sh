bb02f5df408c57c3fe66e6d4dedbb48589e9e528
Anatoli Lichii <anatoli@lanixx.com>
2023-01-04 22:37:17 +0100

    Update tooling.sh

--- examples/tooling.sh
+++ examples/tooling.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 curl --silent --location "https://github.com/lanixx-hh/runfromyaml/releases/latest/download/runfromyaml-$(uname -s)-$(uname -m).tar.gz" | tar xz
 curl --silent --location --output tooling.yaml https://raw.githubusercontent.com/LANIXX-HH/runfromyaml/master/examples/tooling.yaml
-exec ./runfromyaml -file tooling.yaml
