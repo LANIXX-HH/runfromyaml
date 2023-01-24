462f8d2a9600ecc595961d0ced4de2f163e91899
Anatoli Lichii <anatoli.lichii@pexon-consulting.com>
2023-01-25 00:00:31 +0100

    feat(tooling): pre release version

--- examples/tooling-pre.sh
+++ examples/tooling-pre.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl --silent --location "https://github.com/lanixx-hh/runfromyaml/releases/v4.0.0/download/runfromyaml-$(uname -s)-$(uname -m).tar.gz" | tar xz
+curl --silent --location "
+https://github.com/LANIXX-HH/runfromyaml/releases/download/v4.0.0/runfromyaml-$(uname -s)-$(uname -m).tar.gz" | tar xz
 curl --silent --location --output tooling.yaml https://raw.githubusercontent.com/LANIXX-HH/runfromyaml/master/examples/tooling.yaml
