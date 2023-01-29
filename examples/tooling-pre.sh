fd2cc896bd6c3f54bd3c6027ee407e7c1ea4fe54
Anatoli Lichii <anatoli@lanixx.com>
2023-01-29 21:26:15 +0100

    Update tooling-pre.sh

--- examples/tooling-pre.sh
+++ examples/tooling-pre.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl --silent --location "https://github.com/LANIXX-HH/runfromyaml/releases/download/v4.1.2/runfromyaml-$(uname -s)-$(uname -m).tar.gz" | tar xz
+curl --silent --location "https://github.com/LANIXX-HH/runfromyaml/releases/download/v4.1.3/runfromyaml-$(uname -s)-$(uname -m).tar.gz" | tar xz
 curl --silent --location --output tooling.yaml https://raw.githubusercontent.com/LANIXX-HH/runfromyaml/master/examples/tooling.yaml
